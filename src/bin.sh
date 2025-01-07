f439d74c264646c31f9935ddc01a334b298bf58f
Andrei Curelaru <andikod@proton.me>
2025-01-08 00:22:41 +0100

    Update bin.sh

--- src/bin.sh
+++ src/bin.sh
@@ -22,11 +22,14 @@ fi
 # Some article route
 if [[ "$1" == "Release 0.1.0" ]]; then
   clear
-  h1 "-= Release 0.1.0 =-"
+  h1 " -= Release 0.1.0 =-"
   hr "sh:News" "-"
-  p "Because we have to start at some point, the v0.1.0 will be out soon enough."
-  p "The Docs and the Discord took quite some time, but at least they're up."
+  br
+  p "Because we have to start at some point, the v0.1.0 will be out soon enough. 
+  The Docs and the Discord took quite some time, but at least they're up."
+  br
   p "It's not quite ready, but come say hi on Discord ;)"
   br
   p "Draft ${em}by AndiKod${x} - Jan8 25"
+  br
 fi
